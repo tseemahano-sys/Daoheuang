4370268a51d2e6d8a90f4fca432d3ad10eb91cf4
tseemahano-sys <tseemahano@gmail.com>
2025-10-29 00:43:08 -0700

    initial commit

--- routes/server.js
+++ routes/server.js
@@ -1,14 +1,13 @@
-const express = require("express");
-const router = express.Router();
+const genresRoute = require("./genresRoute");
+
+const express = Require("express");
+const router = router("./route/genresRoute");
 
 const app = express();
-const port = 3000;
-
-// Middleware
-app.use(express.json);
+const port = 3000
 
 // Use the imported router for all paths
-app.use("/",router);
+app.use("/",genresRoute);
 
 app.listen(port, () => {
     console.log(`book Library API running at http://localhost:${port}`);
